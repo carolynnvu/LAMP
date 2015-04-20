ac65861c14fff9e44ba0b2e6d8b3b1bac7dac0a8
Carolynn Vu <carolynn.vu@gmail.com>
2015-04-20 19:43:38 -0400

    finish up search result page stuff

--- search_student.php
+++ search_student.php
@@ -23,7 +23,7 @@ if (isset($_POST['submitBtn'])) {
 	$usernameVal = "'".$_POST['username']."'";
 
 	//Setup query 
-	$query = "SELECT first_nm, last_nm, date_of_birth, 
+	$query = "SELECT username, first_nm, last_nm, date_of_birth, 
 			  sex.descript AS sex_descript, 
 			  age_group.descript AS age_descript
 			  FROM students LEFT JOIN sex ON students.sex_id = sex.sex_id 
@@ -46,11 +46,35 @@ if (isset($_POST['submitBtn'])) {
 		<?php 
 			$row_count = mysqli_num_rows($resultSet); 
 			if($row_count < 1) {
-				echo "NO STUDENT";
-			} else {
-				echo "FOUND!!!";
-			}
-		?>
+				echo "No student found in database for given username.";
+			} else { ?> 
+				<table>
+					<thead>
+						<tr>
+							<th>Username</th>
+							<th>First Name</th>					
+							<th>Last Name</th>					
+							<th>DOB</th>					
+							<th>Sex</th>					
+							<th>Age Group</th>
+							<th>See More</th>					
+						</tr>
+					</thead>
+					<tbody>
+						<?php while($row = mysqli_fetch_array($resultSet)) : ?>
+						<tr>
+							<td><?php echo $row["username"]; ?></td>
+							<td><?php echo $row["first_nm"]; ?></td>
+							<td><?php echo $row["last_nm"]; ?></td>
+							<td><?php echo $row["date_of_birth"]; ?></td>
+							<td><?php echo $row["sex_descript"]; ?></td>
+							<td><?php echo $row["age_descript"]; ?></td>
+							<td><a href="student.php?username=<?php echo $row["username"]; ?>"><img src="images/magnifying-glass.png"></a></td>
+						</tr>
+						<?php endwhile; ?>
+					</tbody>
+				</table>
+		<?php } ?>
 	</body>
 </html>
 
